a5f6761706a077adc627db8591e8bcf5a6d7d270
kignatyev <kignatyev@xpansive.com>
2021-08-14 09:36:48 -0700

    added registries API; added option to generate FQDN for data;

--- build.sh
+++ build.sh
@@ -11,13 +11,28 @@ set -x
 
 docker_cmd="docker run --rm  -v $PWD:/usr/src/app -w /usr/src/app $IMAGE"
 
-$docker_cmd \
-   protoc -I /usr/local/include/protos -I /usr/src/app/src \
-   --openapiv2_out /usr/src/app/${OPENAPI_OUT_DIR} --openapiv2_opt logtostderr=true  \
-   xpansiv/ema-admin-api.proto
+convertToSwagger(){
+  $docker_cmd \
+     protoc -I /usr/local/include/protos -I /usr/src/app/src \
+     --openapiv2_out /usr/src/app/${OPENAPI_OUT_DIR} \
+     --openapiv2_opt logtostderr=true  \
+     --openapiv2_opt generate_unbound_methods=true  \
+     --openapiv2_opt fqn_for_openapi_name=true  \
+     xpansiv/${1}.proto
+  jq --arg version $FULL_VERSION '.info.version = $version' openapi/xpansiv/${1}.swagger.json > openapi/xpansiv/${1}.swagger.tmp.json
+}
 
-$docker_cmd \
-   jq --arg version $FULL_VERSION '.info.version = $version' openapi/xpansiv/ema-admin-api.swagger.json > openapi/xpansiv/ema-admin-api.swagger.tmp.json
-cp openapi/xpansiv/ema-admin-api.swagger.tmp.json openapi/xpansiv/ema-admin-api.swagger.json
-cp openapi/xpansiv/ema-admin-api.swagger.tmp.json openapi/xpansiv/ema-admin-api.${FULL_VERSION}.json
-git add openapi/xpansiv/ema-admin-api.${FULL_VERSION}.json
+makeVersionedFile(){
+  cp openapi/xpansiv/${1}.swagger.tmp.json openapi/xpansiv/${1}.swagger.json
+  cp openapi/xpansiv/${1}.swagger.tmp.json openapi/xpansiv/${1}.${FULL_VERSION}.json
+  git add openapi/xpansiv/${1}.${FULL_VERSION}.json
+
+}
+
+convertProto() {
+  convertToSwagger $1
+  makeVersionedFile $1
+}
+
+convertProto ema-admin-api
+convertProto ema-registry-api
