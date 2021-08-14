2aafd7bf155770409acd1bce2b3fdfb2d9d8169d
kignatyev <kignatyev@xpansive.com>
2021-08-14 11:53:25 -0700

    added use of plugin to link to different APIs;

--- run-swagger-ui.sh
+++ run-swagger-ui.sh
@@ -3,6 +3,7 @@
 echo "browse docs on http://localhost:9080"
 
 docker run -p 9080:8080 \
-    -e SWAGGER_JSON=/openapiv2/xpansiv/ema-admin-api.swagger.json \
+    -e URLS='[{url: "xpansiv/ema-admin-api.swagger.json", name: "admin"},{url: "xpansiv/ema-registry-api.swagger.json", name: "registry"}]' \
     -v $PWD/openapi/:/openapiv2 \
+    -v $PWD/openapi/xpansiv:/usr/share/nginx/html/xpansiv \
     swaggerapi/swagger-ui
